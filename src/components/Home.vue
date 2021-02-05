fa4fa72eb9337638562eb8aaac6a3174c6bdf5ac
saimyominlatt <saimyominlatt@saimyominlatt.me>
2021-02-05 14:20:24 +0630

    mm

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <h1>Hello Home Page</h1>
+  <p>{{name}}</p>
+</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      name: "Home Msai"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
